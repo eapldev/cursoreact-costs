76698719a9711c7e7d05e4fda0dda5f90236a8b7
eapldev <eduardoaparecido.dev@gmail.com>
2024-02-23 09:53:41 -0300

    Página para o formulário de projetos

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,14 @@
+import ProjectForm from '../projects/ProjectForm'
+import styles from './NewProjects.module.css'
+
 function NewProject() {
-    return <h1>NewProject</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm />
+        </div>
+    )
 }
 
 export default NewProject
